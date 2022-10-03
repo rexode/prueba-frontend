825dff1953a50a904561e11db389f1794ad7e41d
rexode <santiago.garcia.castillo@outlook.com>
2022-10-03 18:42:38 +0200

    inicio results

--- web/src/components/Lottery.jsx
+++ web/src/components/Lottery.jsx
@@ -14,7 +14,6 @@ import { Box } from "@mui/system";
 import { Refresh, Add, Remove } from "@mui/icons-material";
 import abi from "../abi/abi.json";
 import { styled } from "@mui/material";
-import { useTry, useTryAsync } from "no-try";
 
 export default function Lottery(props) {
   const [Nwinners, setNwinners] = useState("Refresh");
@@ -24,14 +23,24 @@ export default function Lottery(props) {
   const [MaxTicketsPlayers, setMaxTicketsPlayers] = useState("Refresh");
   const [CostTicket, setCostTicket] = useState("Refresh");
   const [IdGame, setIdGame] = useState("Refresh");
+  const [Pool, setPool] = useState("Refresh");
+
   const [buttonState, setButtonState] = useState("loaded");
   const [active, setActive] = useState(false);
   const { account, provider } = props;
   const [error, setError] = useState("succesfull");
   const [open, setOpen] = useState(false);
+  const [Success, setSuccess] = useState(false);
+
   const [NTickets, setNTickets] = useState(parseInt(0));
   const [TotalTicketsUser, setTotalTicketsUser] = useState(parseInt(0));
-  let PercentageWinners=[[0.8],[0.6,0.2],[0.5,0.2,0.1],[0.50,0.10,0.10,0.5,0.5]]
+  let PercentageWinners = [
+    [],
+    [0.8],
+    [0.6, 0.2],
+    [0.5, 0.2, 0.1],
+    [0.5, 0.1, 0.1, 0.5, 0.5],
+  ];
 
   const Aeth = 10 ** 18;
 
@@ -43,50 +52,117 @@ export default function Lottery(props) {
   });
 
   const getInfoContract = async () => {
-    setButtonState("loading");
-    const contract = new ethers.Contract(
-      "0x925463A53124b23B5162ad1C3dCffcBcd6BEE040",
-      abi,
-      provider
-    );
-    const temIdGame = await contract.idGame();
-    console.log(temIdGame.toString());
-    setIdGame(parseInt(temIdGame));
+    if(provider==null){
+      setError("Connect to Blockchain");
+      setOpen(true);
+    }else{
+      setButtonState("loading");
+      const contract = new ethers.Contract(
+        "0x925463A53124b23B5162ad1C3dCffcBcd6BEE040",
+        abi,
+        provider
+      );
+      const temIdGame = await contract.idGame();
+      console.log(temIdGame.toString());
+      setIdGame(parseInt(temIdGame));
 
-    const temMaxTicketsPerPlayer = await contract.maxTicketsPlayer();
-    console.log(temMaxTicketsPerPlayer.toString());
-    setMaxTicketsPlayers(parseInt(temMaxTicketsPerPlayer));
+      const temMaxTicketsPerPlayer = await contract.maxTicketsPlayer();
+      console.log(temMaxTicketsPerPlayer.toString());
+      setMaxTicketsPlayers(parseInt(temMaxTicketsPerPlayer));
 
-    const temNwinners = await contract.Nwinners(temIdGame);
-    console.log(temNwinners.toString());
-    setNwinners(parseInt(temNwinners));
+      const temNwinners = await contract.Nwinners(temIdGame);
+      console.log(temNwinners.toString());
+      setNwinners(parseInt(temNwinners));
 
-    const temCost = (await contract.costTicket()) / Aeth;
-    console.log(temCost.toString());
-    setCostTicket(temCost);
+      const temCost = (await contract.costTicket()) / Aeth;
+      console.log(temCost.toString());
+      setCostTicket(temCost);
 
-    const temPrize = (await contract.prize()) / Aeth;
-    console.log(temPrize.toString());
-    setPrize(temPrize);
+      const temPrize = (await contract.prize()) / Aeth;
+      console.log(temPrize.toString());
+      setPrize(temPrize);
 
-    const temMaxTickets = await contract.maxTickets();
-    console.log(temMaxTickets.toString());
-    setMaxTickets(parseInt(temMaxTickets));
+      const temPool = (await contract.pool(temIdGame)) / Aeth;
+      console.log(temPool.toString());
+      setPool(temPool);
 
-    const temTotalTicket = await contract.totalTickets();
-    console.log(parseInt(temMaxTickets) - parseInt(temTotalTicket));
-    setTicktetsLeft(parseInt(temMaxTickets) - parseInt(temTotalTicket));
+      const temMaxTickets = await contract.maxTickets();
+      console.log(temMaxTickets.toString());
+      setMaxTickets(parseInt(temMaxTickets));
 
-    const temActive = await contract.active();
-    console.log(temActive.toString());
-    setActive(temActive);
+      const temTotalTicket = await contract.totalTickets();
+      console.log(parseInt(temMaxTickets) - parseInt(temTotalTicket));
+      setTicktetsLeft(parseInt(temMaxTickets) - parseInt(temTotalTicket));
 
-    const temTotalTicketUser = await contract.NTicketsOwner(account);
-    console.log(temTotalTicketUser.toString());
-    setTotalTicketsUser(parseInt(temTotalTicketUser));
-    setNTickets(parseInt(0));
-    setButtonState("loaded");
+      const temActive = await contract.active();
+      console.log(temActive.toString());
+      setActive(temActive);
+
+      const temTotalTicketUser = await contract.NTicketsOwner(account);
+      console.log(temTotalTicketUser.toString());
+      setTotalTicketsUser(parseInt(temTotalTicketUser));
+      setNTickets(parseInt(0));
+      setButtonState("loaded");
+    }
   };
+
+  function showLottery() {
+    let PrizeWinners = [];
+    for (let i = 0; i < Nwinners; i++) {
+      PrizeWinners.push((i+1)+"º Premio: "+(Pool * PercentageWinners[Nwinners][i]).toFixed(2).toString());
+    }
+
+    return (
+      <Card raised sx={{ width: 1 / 2 }}>
+        <Typography variant="h1">Prize:{Prize}</Typography>
+        <Typography>id:{IdGame}</Typography>
+        <Typography>Nº Winners:{Nwinners}</Typography>
+        <Typography>Cost Ticket:{CostTicket}</Typography>
+        <Typography>Max Tickets:{MaxTickets}</Typography>
+        <Typography>Tickets left:{TicktetsLeft}</Typography>
+        <Typography>Max Tickets/wallet:{MaxTicketsPlayers}</Typography>
+        <Typography>
+          TicketsAlreadyBought :{TotalTicketsUser}/{MaxTicketsPlayers}
+        </Typography>
+        {PrizeWinners.map((winner) => (
+          <Typography>{winner}</Typography>
+        ))}
+        {active ? (
+          <Box>
+            <ButtonGroup variant="outlined" aria-label="outlined button group">
+              <Button
+                onClick={() => {
+                  ChangeNtickets(true);
+                }}
+              >
+                <Add />
+              </Button>
+              <Button>{NTickets}</Button>
+              <Button
+                onClick={() => {
+                  ChangeNtickets(false);
+                }}
+              >
+                <Remove />
+              </Button>
+            </ButtonGroup>
+            <Box sx={{ mt: 3 }}>
+              <BotonPersonalizado onClick={pruebaError}>
+                prueba
+              </BotonPersonalizado>
+              
+            </Box>
+          </Box>
+        ) : (
+          <Grid container direction="row" justifyContent="center" spacing={12}>
+            <Grid item>
+              <BotonPersonalizado disabled>Lottery ended</BotonPersonalizado>
+            </Grid>
+          </Grid>
+        )}
+      </Card>
+    );
+  }
 
   const prueba = () => {
     console.log(TicktetsLeft.toString());
@@ -103,7 +179,7 @@ export default function Lottery(props) {
       setNTickets(NTickets + 1);
     } else if (!boolean && NTickets > 0) {
       setNTickets(NTickets - 1);
-    }else if(boolean && NTickets + 1 > MaxTicketsPlayers - TotalTicketsUser){
+    } else if (boolean && NTickets + 1 > MaxTicketsPlayers - TotalTicketsUser) {
       setError("You can't buy more");
       setOpen(true);
     }
@@ -129,6 +205,7 @@ export default function Lottery(props) {
         await contract.join(NTickets, {
           value: ethers.utils.parseEther((0.01 * NTickets).toString()),
         });
+        setSuccess(true);
       } catch (e) {
         setError(e.reason.substring(e.reason.indexOf(": ") + 1));
         setOpen(true);
@@ -145,112 +222,59 @@ export default function Lottery(props) {
 
     setOpen(false);
   };
+  const handleCloseSuccess = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+
+    setSuccess(false);
+  };
 
   return (
     <Box>
-      {account == null ? (
-        <Typography>Connect to the blockchain</Typography>
-      ) : (
-        <Box>
-          <Button
-            onClick={getInfoContract}
-            disabled={buttonState === "loading"}
+      {open ? (
+        <Snackbar open={open} autoHideDuration={6000} onClose={handleClose}>
+          <Alert
+            onClose={handleClose}
+            severity="warning"
+            sx={{ width: "100%" }}
           >
-            <Refresh />
-          {buttonState === "loaded" ? "Refresh" : "Fetching..."}
-          </Button>
-          {buttonState === "loaded" ? (
-            <Box sx={{ display: "flex", justifyContent: "center" }}>
-              <Card raised sx={{ width: 1 / 2 }}>
-                <Typography variant="h1">Prize:{Prize}</Typography>
-                <Typography>id:{IdGame}</Typography>
-                <Typography>Nº Winners:{Nwinners}</Typography>
-                <Typography>Cost Ticket:{CostTicket}</Typography>
-                <Typography>Max Tickets:{MaxTickets}</Typography>
-                <Typography>Tickets left:{TicktetsLeft}</Typography>
-                <Typography>Max Tickets/wallet:{MaxTicketsPlayers}</Typography>
-                <Typography>
-                  TicketsAlreadyBought :{TotalTicketsUser}/{MaxTicketsPlayers}
-                </Typography>
-                {active ? (
-                  <Box>
-                    <ButtonGroup
-                      variant="outlined"
-                      aria-label="outlined button group"
-                    >
-                      <Button
-                        onClick={() => {
-                          ChangeNtickets(true);
-                        }}
-                      >
-                        <Add />
-                      </Button>
-                      <Button>{NTickets}</Button>
-                      <Button
-                        onClick={() => {
-                          ChangeNtickets(false);
-                        }}
-                      >
-                        <Remove />
-                      </Button>
-                    </ButtonGroup>
-                    <Box sx={{ mt: 3 }}>
-                      <BotonPersonalizado onClick={pruebaError}>
-                        prueba
-                      </BotonPersonalizado>
-                      {open ? (
-                        <Snackbar
-                          open={open}
-                          autoHideDuration={6000}
-                          onClose={handleClose}
-                        >
-                          <Alert
-                            onClose={handleClose}
-                            severity="warning"
-                            sx={{ width: "100%" }}
-                          >
-                            {error}
-                          </Alert>
-                        </Snackbar>
-                      ) : (
-                        <Snackbar
-                          open={open}
-                          autoHideDuration={6000}
-                          onClose={handleClose}
-                        >
-                          <Alert
-                            onClose={handleClose}
-                            severity="success"
-                            sx={{ width: "100%" }}
-                          >
-                            This is a success message!
-                          </Alert>
-                        </Snackbar>
-                      )}
-                    </Box>
-                  </Box>
-                ) : (
-                  <Grid
-                    container
-                    direction="row"
-                    justifyContent="center"
-                    spacing={12}
-                  >
-                    <Grid item>
-                      <BotonPersonalizado disabled>
-                        Lottery ended
-                      </BotonPersonalizado>
-                    </Grid>
-                  </Grid>
-                )}
-              </Card>
-            </Box>
-          ) : (
-            <></>
-          )}
-          <Box></Box>
-        </Box>
+            {error}
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
       )}
+      {Success ? (
+        <Snackbar
+          open={Success}
+          autoHideDuration={6000}
+          onClose={handleCloseSuccess}
+        >
+          <Alert
+            onClose={handleCloseSuccess}
+            severity="success"
+            sx={{ width: "100%" }}
+          >
+            Success!!
+          </Alert>
+        </Snackbar>
+      ) : (
+        <></>
+      )}
+      <Box>
+        <Button onClick={getInfoContract} disabled={buttonState === "loading"}>
+          <Refresh />
+          {buttonState === "loaded" ? "Refresh" : "Fetching..."}
+        </Button>
+        {buttonState === "loaded" ? (
+          <Box sx={{ display: "flex", justifyContent: "center" }}>
+            {showLottery()}
+          </Box>
+        ) : (
+          <></>
+        )}
+      </Box>
     </Box>
   );
 }
--- web/src/components/Results.jsx
+++ web/src/components/Results.jsx
@@ -4,27 +4,40 @@ import { useEffect, useState } from "react";
 import { ethers } from "ethers";
 import abi from "../abi/abi.json";
 
-
 export default function (props) {
   const { account, provider } = props;
   const [IdGame, setIdGame] = useState("Refresh");
   const [active, setActive] = useState(false);
+  let winners = [][20];
+  let Nwinners = [];
   const getInfoContract = async () => {
     const contract = new ethers.Contract(
-        "0x97a3e38711404202Fc432555aC8c33FFE78558d1",
-        abi,
-        provider
-      );
-      const temActive = await contract.active();
+      "0x97a3e38711404202Fc432555aC8c33FFE78558d1",
+      abi,
+      provider
+    );
+    const temActive = await contract.active();
     console.log(temActive.toString());
     setActive(temActive);
 
     const temIdGame = await contract.idGame();
     console.log(temIdGame.toString());
     setIdGame(parseInt(temIdGame));
-      
-      
 
-
+    let lastGameId = active ? temIdGame - 1 : temIdGame;
+    if (lastGameId > 1) {
+      for (var i = lastGameId; i > lastGameId-3 && i>0; i--) {
+        var temNwinners = await contract.Nwinners(i);
+        console.log(temIdGame.toString());
+        Nwinners[i] = temNwinners;
+        for (var j =0 ; i < temNwinners; i++) {
+          var temWinners = await contract.winners(i,j);
+          winners.push(temWinners);
+        }
+      }
+    }
   };
+  return(
+    <></>
+  )
 }
